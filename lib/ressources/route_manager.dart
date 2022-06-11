9a95f07183fdc92ef61847bea644ac9843301997
meymed2727 <meymed.aly@gmail.com>
2022-06-11 11:43:23 +0000

    des mdf sur les routes

--- lib/ressources/route_manager.dart
+++ lib/ressources/route_manager.dart
@@ -4,20 +4,33 @@ import 'package:get/get.dart';
 import 'package:testessava/splash/splash_view.dart';
 import 'package:testessava/views/home/hom_view.dart';
 
+import '../views/addpdv/addpdv_view.dart';
+import '../views/modifpdv/modifpdv_view.dart';
+
 class RoutesName {
   static const splashRoute = '/';
   static const homeRoute = '/homeRoutes';
+  static const modifPdvRoute = '/modifPdvRoute';
+  static const addPdvRoute = '/addPdvRoute';
 }
 
 class RoutesPages {
   static final pages = [
     GetPage(
       name: RoutesName.splashRoute,
-      page: () => SplashView(),
+      page: () => const SplashView(),
     ),
     GetPage(
       name: RoutesName.homeRoute,
-      page: () => HomeView(),
+      page: () => const HomeView(),
+    ),
+    GetPage(
+      name: RoutesName.addPdvRoute,
+      page: () => const AddPdvView(),
+    ),
+    GetPage(
+      name: RoutesName.modifPdvRoute,
+      page: () => const ModifierPdvView(),
     )
   ];
 }
---
+++ lib/views/modifpdv/modifpdv_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ModifierPdvView extends StatelessWidget {
+  const ModifierPdvView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
